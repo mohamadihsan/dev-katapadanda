c110d9027484ada74c910b43a7b2a59ff2d19c60
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-26 09:22:05 +0700

    change

--- resources/views/web_katapanda.blade.php
+++ resources/views/web_katapanda.blade.php
@@ -46,7 +46,7 @@
             </button>
               <a class="navbar-brand page-scroll" href="#main"><img src="{{ asset('assets_frontend/logos/logo.png') }}" alt="Mash Able Logo" /></a>
               </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
+              
               <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li><a class="page-scroll" href="#main">Beranda</a></li>
